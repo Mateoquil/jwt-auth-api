f6cf479af2039853a0d4b2c6ccf78c90c177b760
mateo <mateocquil@gmail.com>
2026-05-26 21:42:08 -0300

    feat: implement bcrypt hashing and async/await for user signup
    - Hash user passwords in model before saving to the database.
    - Refactor registration controller to use async/await with Promise queries.

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,10 +1,20 @@
-let Register = [];
-let users = [];
+// user.model.js
+import bcrypt from 'bcrypt';
+import connection from '../config/db.js';
 
 export default {
-    create: (email, password) => {
-        const newUser = { email, password };
-        users.push(newUser);
-        return newUser;
+    create: async (email, password) => {
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+        return new Promise((resolve, reject) => {
+            connection.query(
+                'INSERT INTO users (email, password) VALUES (?, ?)',
+                [email, password],
+                (err, result) => {
+                    if (err) reject(err);
+                    else resolve(result);
+                }
+            );
+        });
     }
 };
