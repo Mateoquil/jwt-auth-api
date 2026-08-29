9c3ddbdf2cdbd2587ca03343aff891a52c359104
mateo <mateocquil@gmail.com>
2026-08-29 20:39:22 -0300

    fix: add CORS support for frontend requests
    - Added cors middleware to allow requests from the POS frontend

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import authRouter from './src/routes/authRoutes.js';
 import connection from './src/database/database.js';
@@ -7,6 +8,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/auth', authRouter);
 
